21a5d2ed3b7a73811f8ad996ba937ece8f724213
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-08-04 12:57:36 +0800

    Create author_sol.cpp

---
+++ author_sol.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n;
+		vector<int> even, odd;
+		for (int i = 0; i < n; i++) {
+			int x;
+			cin >> x;
+			if (x % 2 == 0) {
+				even.push_back(x);
+			} else {
+				odd.push_back(x);
+			}
+		}
+		// last bit:
+		// even numbers = 0
+		// odd numbers = 1
+		// to form the correct sequence, all the even numbers must be printed first
+		// before all the odd numbers (this will create a minimum ugliness value since
+		// in the first half, there will be no increase since all even numbers have 0
+		// on their last bit that's why no consecutive 1s will be formed)
+		for (int i = 0; i < (int) even.size(); i++) {
+			cout << even[i] << " ";
+		}
+		for (int i = 0; i < (int) odd.size(); i++) {
+			cout << odd[i] << " ";
+		}
+		cout << '\n';
+	}
+	return 0;
+}
